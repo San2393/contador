c5c333384a3eb584b3eb1654e9ce692f6224daac
Santiago Herrera <santiagoherrera@seven4n.com>
2019-07-24 21:56:54 -0500

    Initial commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final estiloTexto = new TextStyle(fontSize: 25);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Titulo"),
+        centerTitle: true,
+        elevation: 10,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Numero de clicks", style: estiloTexto),
+            Text("0", style: estiloTexto),
+          ],
+        ),
+      ),
+    );
+  }
+}
