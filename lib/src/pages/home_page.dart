21af629be2e785e3db25fdd157afa38a6bc96cc1
Santiago Herrera <santiagoherrera@seven4n.com>
2019-07-29 20:33:42 -0500

    Se agregan botones y funciones

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final estiloTexto = new TextStyle(fontSize: 25);
+  final int conteo = 10;
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +17,15 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text("Numero de clicks", style: estiloTexto),
-            Text("0", style: estiloTexto),
+            Text('$conteo', style: estiloTexto),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // conteo++;
+        },
       ),
     );
   }
